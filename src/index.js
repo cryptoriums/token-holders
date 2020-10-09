b2274865b9643fbf7095d65507f6ded9501b9dda
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2020-10-09 11:50:36 +0300

    ignore fields with empty token name or address
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -105,9 +105,17 @@ class Main extends FormsManager {
     let setup = loadState("setup")[0]
 
     let tokens = []
+    let hasEmpty
     stateTokens.forEach(token => {
+      if (token.address === "" || token.name === "") {
+        hasEmpty = true
+        return
+      }
       tokens.push(new Token(token.name, token.address))
     })
+    if (hasEmpty != undefined) {
+      alert("tokens with empty name or addres are ignored")
+    }
 
     let singles = async () => {
       let name = "SINGLE TOKENS"
@@ -201,7 +209,7 @@ class Main extends FormsManager {
             <h2>{provider.providerName}</h2><br />
             {provider.tokenHolders.map((token) => {
               return <div>
-                {((token.contracts !== undefined && token.contracts.length > 0) || token.error !== undefined) && <h4 style={{ margin: 0 }}>  {token.name} < br /></h4>}
+                {(token.contracts.length > 0 || token.error !== undefined) && <h4 style={{ margin: 0 }}>  {token.name} < br /></h4>}
                 {token.error !== undefined ? < div > {token.error}</div> : token.contracts.map((contract) => {
                   return <div style={{ clear: 'both' }}>
                     <a style={{ width: 24 + 'em', display: 'block', float: 'left' }} target="_blank" rel="noopener noreferrer" href={"https://etherscan.io/address/" + contract.address}>{contract.address}</a>
